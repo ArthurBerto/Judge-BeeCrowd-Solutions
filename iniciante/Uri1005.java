7f1f4ef9054cb325c99b25e0f4392d5800d06bc1
Arthur Berto <77360450+ArthurBerto@users.noreply.github.com>
2024-07-27 12:55:12 -0300

    Updating exercise 5

--- iniciante/Uri1005.java
+++ iniciante/Uri1005.java
@@ -2,6 +2,27 @@ package iniciante;
 
 import java.util.Scanner;
 
+/*
+
+QUESTÃO
+Leia 2 valores de ponto flutuante de dupla precisão A e B, que 
+correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno,
+sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos 
+pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre 
+com uma casa decimal.
+
+ENTRADA
+O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+
+SAÍDA
+Calcule e imprima a variável MEDIA conforme exemplo abaixo, com 5 dígitos
+após o ponto decimal e com um espaço em branco antes e depois da igualdade.
+Utilize variáveis de dupla precisão (double) e como todos os problemas, não
+esqueça de imprimir o fim de linha após o resultado, caso contrário, você
+receberá "Presentation Error".
+
+*/
+
 public class Uri1005 {
 
 	public static void main(String[] args) {
@@ -14,7 +35,7 @@ public class Uri1005 {
 		nota2 = sc.nextDouble();
 		
 		media = ((nota1 * 3.5) + (nota2 * 7.5)) / (3.5 + 7.5);
-		System.out.println(String.format("MEDIA = %.4f", media));
+		System.out.println(String.format("MEDIA = %.5f", media));
 		
 		sc.close();
 
